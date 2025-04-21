1d81b859e0109308043ad7c2553fc2515046f145
Zaa <zaa_ramirez@outlook.com>
2025-04-20 19:44:26 -0600

    Codigo sin buenas practicas

---
+++ SistemaGimnasio/NoBueno.cs
@@ -0,0 +1,60 @@
+﻿public class Rutina
+{
+    public string nombre;
+    public int duracion;
+    public List<string> ejercicios = new List<string>();
+
+    public void AgregarEjercicio(string e)
+    {
+        ejercicios.Add(e);
+    }
+}
+
+public class Usuario
+{
+    public string n;
+    public int e;
+    public string o;
+    public Rutina r;
+}
+
+public class Entrenador
+{
+    public string nom;
+    public string esp;
+    public List<Usuario> usuarios = new List<Usuario>();
+}
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Bienvenido al sistema");
+
+    Usuario u = new Usuario();
+    u.n = "Pepe";
+    u.e = 25;
+    u.o = "Fuerza";
+
+    Rutina r = new Rutina();
+    r.nombre = "Rutina1";
+    r.duracion = 30;
+    r.AgregarEjercicio("Press banca");
+    r.AgregarEjercicio("Sentadillas");
+    r.AgregarEjercicio("Abdominales");
+
+    u.r = r;
+
+    Entrenador t = new Entrenador();
+    t.nom = "Luis";
+    t.esp = "Fuerza";
+    t.usuarios.Add(u);
+
+    Console.WriteLine("Usuario: " + u.n);
+    Console.WriteLine("Edad: " + u.e);
+    Console.WriteLine("Objetivo: " + u.o);
+    Console.WriteLine("Rutina asignada: " + u.r.nombre + " (" + u.r.duracion + " mins)");
+    foreach (var x in r.ejercicios)
+    {
+        Console.WriteLine("Ejercicio: " + x);
+    }
+
+    Console.WriteLine("Entrenador: " + t.nom + " (" + t.esp + ")");
